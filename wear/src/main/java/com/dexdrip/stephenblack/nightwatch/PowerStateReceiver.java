46226d7c525b2dc3c70bf614b001c1f6f1fc75ef
AdrianLxM <adrian.m@inbox.com>
2015-12-02 02:32:04 +0100

    BroadcastReceiver for ACTION_POWER_CONNECTED

---
+++ wear/src/main/java/com/dexdrip/stephenblack/nightwatch/PowerStateReceiver.java
@@ -0,0 +1,29 @@
+package com.dexdrip.stephenblack.nightwatch;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * Created by adrian on 02/12/15.
+ */
+public class PowerStateReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        String action = intent.getAction();
+        if (action.equals(Intent.ACTION_POWER_CONNECTED)) {
+            startStopActivity(true,NWPreferences.class,context);
+        } else if (action.equals(Intent.ACTION_POWER_DISCONNECTED)) {
+            startStopActivity(false, NWPreferences.class, context);
+        }
+    }
+
+    private void startStopActivity(boolean start, Class activityClass, Context context){
+        Intent intent = new Intent(context, activityClass);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        intent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
+        intent.putExtra("start", start);
+        context.startActivity(intent);
+    }
+}
